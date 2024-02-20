bcd54c5679cba5bffdd7c52692bb88ca353ef2c3
slim20000 <slim.derouiche@esprit.tn>
2024-02-20 11:48:31 +0100

    Initial commit

---
+++ src/main/java/com/slim/acterio/ActerioCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.slim.acterio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActerioCodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActerioCodingChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slim/acterio/ActerioCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slim.acterio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActerioCodingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
